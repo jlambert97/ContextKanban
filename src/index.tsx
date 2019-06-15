bfe08ee0af0cba169be1d930a79f1014d4777d65
jlambert97 <joao.lambert@outlook.com>
2019-06-15 02:36:33 -0300

    ReduxDevTools xd

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,12 @@ import { AppState } from './CombineReducers'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 
+
+const devTools = (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+(window as any).__REDUX_DEVTOOLS_EXTENSION__()
+
 ReactDOM.render(
-    <Provider store={createStore(AppState)}>
+    <Provider store={createStore(AppState, devTools)}>
         <App />
     </Provider>
 , document.getElementById('root'));

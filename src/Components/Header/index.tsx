e6254ef2c718dd6a161bd6469d3fa19a16b8461b
joao.lambert <joao.lambert@ciashop.com.br>
2019-08-06 17:41:01 -0300

    axios

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Label } from './style'
+import Axios from 'axios'
 
 const Header = () => {
+    const [ val, setVal ] = useState('Sem nada')
+
+    const handleButton = () => {
+        Axios.get('http://saleshub.ciashop.com.br/api/v1/healthcheck') 
+            .then(res => {
+                console.log(res.data)
+                setVal(res.data[3].Service)})
+    }
+    
     return (
         <Container>
-            <Label>KanbanContext</Label>
+            <Label>KanbanContext {val}</Label>
+            <button onClick={() => handleButton()}>Xaxa</button>
         </Container>
     )
 }
